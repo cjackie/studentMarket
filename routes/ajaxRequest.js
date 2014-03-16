2b41c03ccec7f10c9fbc18d084a2a0cea815406a
cjackie <chaojie.keepgoing@gmail.com>
2014-03-16 11:58:12 -0400

    random ajax requests for all pages

---
+++ routes/ajaxRequest.js
@@ -0,0 +1,28 @@
+
+
+
+//models
+var Book = require('./models/Book');
+var User = require('./models/User');
+var books = new Book();
+var users = new User();
+
+/*
+  check if username has been register
+ */
+
+exports.testUsername = function(req, res){
+    var username = req.query.username;
+    console.log(username);
+    if (!username){
+        res.json({availibility: 'no'});
+    }
+    
+    users.findByName(username, function(err, data){
+        if (err || data){
+            res.json({availibility: 'no'});
+        } else {
+            res.json({availibility: 'yes'});
+        }
+    });
+};
