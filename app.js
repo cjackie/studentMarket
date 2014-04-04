0be652d314433da4e6c1af085c8a8e20ea587b35
cjackie <chaojie.keepgoing@gmail.com>
2014-04-04 16:19:13 -0400

    dd

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -95,8 +94,6 @@ app.get('/error', routes.error);
 //when urls are not found
 /* TODO */
 
-
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
